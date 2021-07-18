32bf8de565b5a73333fcdc705283328a6e01afd9
AlixRomain <romain.alix89@gmail.com>
2021-07-18 20:01:33 +0200

    add a attachment link between  users and Task

--- src/Form/FilterType.php
+++ src/Form/FilterType.php
@@ -14,10 +14,19 @@ class FilterType extends AbstractType
         $builder
             ->add('filter', ChoiceType::class, [
                 'choices'  => [
-                    'Deadline' => 0,
+                    'Ranger par Deadline' => 0,
                     'Toutes'=> 'all',
                     'Terminé' => 2,
                     'Encours' => 3,
+                    'Qui me sont destinés' => 4
+
+                ],
+                'choice_attr' =>[
+                    'Ranger par Deadline' => ['class' => 'optio'],
+                    'Toutes' => ['class' => 'optio'],
+                    'Terminé' => ['class' => 'optio'],
+                    'Encours' => ['class' => 'optio'],
+                    'Qui me sont destinés' => ['class' => 'optio'],
                 ],
                 'label' => "Filtres de tâches",
                 'attr' => [
@@ -25,7 +34,6 @@ class FilterType extends AbstractType
                     'class' =>'form-control d-inline ',
                 ]
             ]);
-        ;
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Task;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,21 +14,49 @@ class TaskType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $choice = [];
+        $choiceClass=[];
+     /*   array_push($choice,[
+            'Ouverte à tous' =>  'default'
+        ]);*/
+        foreach ($options['users'] as $val){
+            array_push($choice,[ $val->getName() =>  $val]);
+            array_push($choiceClass, [$val->getName() => ['class' => 'optio']]);
+        };
+
         $builder
             ->add('title', TextType::class, [
                 'label' => "Titre",
                 'attr' => [
                     'placeholder' => 'Votre titre',
-                    'class' =>'form-control',
+                    'class' =>'form-control mb-3',
+                ]
+            ])
+            ->add('deadline', DateType::class, [
+                'widget' => 'choice',
+                'attr' => [
+                    'placeholder' => 'Votre titre',
+                    'class' =>'form-control mb-3',
                 ]
             ])
            ->add('content', TextType::class, [
-                'label' => "Titre",
+                'label' => "Contenus",
                 'attr' => [
                     'placeholder' => 'Votre contenu',
-                    'class' =>'form-control',
+                    'class' =>'form-control mb-3',
                 ]
             ])
+            ->add('targetUser', ChoiceType::class, [
+                'required'   => false,
+                'empty_data' => 'default',
+                'choices'  => $choice,
+                'label' => "A qui voulez-vous assigner la tâche?",
+                'attr' => [
+                    'placeholder' => 'Filtré les tâches',
+                    'class' =>'form-control d-inline mb-3',
+                ],
+                'choice_attr' => $choiceClass,
+            ]);
 
         ;
     }
@@ -36,5 +66,8 @@ class TaskType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Task::class,
         ]);
+        $resolver->setRequired([
+                                   'users'
+                               ]);
     }
 }
