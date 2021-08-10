011d6dc224727d5df486f5cbece4ae86bac81ba2
AlixRomain <romain.alix89@gmail.com>
2021-08-10 11:45:16 +0200

    update file for set a command

--- Command/StartTestCommand.php
+++ Command/StartTestCommand.php
@@ -48,86 +48,86 @@ class CreateUserCommand extends Command implements KernelInterface
 
     public function handle(Request $request, int $type = self::MAIN_REQUEST, bool $catch = true)
     {
-        // TODO: Implement handle() method.
+
     }
 
     public function registerBundles()
     {
-        // TODO: Implement registerBundles() method.
+
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        // TODO: Implement registerContainerConfiguration() method.
+
     }
 
     public function boot()
     {
-        // TODO: Implement boot() method.
+
     }
 
     public function shutdown()
     {
-        // TODO: Implement shutdown() method.
+
     }
 
     public function getBundles()
     {
-        // TODO: Implement getBundles() method.
+
     }
 
     public function getBundle(string $name)
     {
-        // TODO: Implement getBundle() method.
+
     }
 
     public function locateResource(string $name)
     {
-        // TODO: Implement locateResource() method.
+
     }
 
     public function getEnvironment()
     {
-        // TODO: Implement getEnvironment() method.
+
     }
 
     public function isDebug()
     {
-        // TODO: Implement isDebug() method.
+
     }
 
     public function getProjectDir()
     {
-        // TODO: Implement getProjectDir() method.
+
     }
 
     public function getContainer()
     {
-        // TODO: Implement getContainer() method.
+
     }
 
     public function getStartTime()
     {
-        // TODO: Implement getStartTime() method.
+
     }
 
     public function getCacheDir()
     {
-        // TODO: Implement getCacheDir() method.
+
     }
 
     public function getLogDir()
     {
-        // TODO: Implement getLogDir() method.
+
     }
 
     public function getCharset()
     {
-        // TODO: Implement getCharset() method.
+
     }
 
     public function __call($name, $arguments)
     {
-        // TODO: Implement @method string getBuildDir()
+
     }
 }
